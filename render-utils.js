5d2cfdc6d006da68d020826389162f809197ae73
Delaney Fogarty <delaney.fk@gmail.com>
2022-04-05 09:40:31 -0700

    renderRating function

--- render-utils.js
+++ render-utils.js
@@ -1,4 +1,6 @@
 import { formatDate } from './date-utils.js';
+import { decrementRating, incrementRating } from './fetch-utils.js';
+import { fetchAndDisplay } from '../employee';
 
 export function renderMessages(profiles) {
     const messagesEl = document.createElement('div');
@@ -18,7 +20,7 @@ export function renderMessages(profiles) {
         fromEl.textContent = `${message.from_email}`;
         atEl.textContent = formatDate(message.created_at);
         textEl.textContent = message.text;
-        
+
         fromEl.classList.add('from');
         textEl.classList.add('text');
         atEl.classList.add('at');
@@ -29,4 +31,30 @@ export function renderMessages(profiles) {
         messagesEl.append(messageEl);
     }
     return messagesEl;
+}
+
+export function renderRating(profile) {
+    const pTag = document.createElement('p');
+    const downButton = document.createElement('button');
+    const upButton = document.createElement('button');
+    const profileRatingEl = document.createElement('div');
+
+    profileRatingEl.classList.add('profile-rating');
+    profileRatingEl.append(pTag, upButton, downButton);
+
+    downButton.textContent = '🤬';
+    upButton.textContent = '😇';
+    pTag.classList.add('profile-name');
+    pTag.textContent = `${profile.email} has ${profile.karma}🤑`;
+
+    downButton.addEventListener('click', async () => {
+        await decrementRating(profile.id);
+        await fetchAndDisplay();
+    });
+    upButton.addEventListener('click', async () => {
+        await incrementRating(profile.id);
+        await fetchAndDisplay();
+    });
+
+    return profileRatingEl;
 }
