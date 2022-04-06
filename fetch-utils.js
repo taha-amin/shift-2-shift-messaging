57bf3400c5bee22f423a991573b0a287125a7c4b
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-04-06 09:36:40 -0700

    added async func sendChat in fetch-utils.js

--- fetch-utils.js
+++ fetch-utils.js
@@ -5,6 +5,18 @@ const SUPABASE_URL = 'https://wkkubdeijvrntdmunqer.supabase.co';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
+//
+export async funciton sendChat(someMessage) {
+    const response = await client
+        .from()
+        .insert({
+            text: someMessage
+        })
+        .single()
+
+    return checkError(response);
+}
+
 export async function getProfiles() {
     const response = await client
         .from('profiles')
